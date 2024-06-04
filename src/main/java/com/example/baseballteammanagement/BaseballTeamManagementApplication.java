c915da45b4de8c35c464e8048b9c76545f743c77
ThanhDat75 <thebestdragonov@gmail.com>
2024-06-04 23:00:09 +0700

    add postman json

--- src/main/java/com/example/baseballteammanagement/BaseballTeamManagementApplication.java
+++ src/main/java/com/example/baseballteammanagement/BaseballTeamManagementApplication.java
@@ -2,13 +2,10 @@ package com.example.baseballteammanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class BaseballTeamManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BaseballTeamManagementApplication.class, args);
     }
-
 }
